41264a06c2369bc130632bd5cf013f47178d3486
walissonestudando <walissonestudando@gmail.com>
2025-10-06 18:12:01 +0000

    criação dos endpoints de usuario

---
+++ src/routes/usuarioRoutes.ts
@@ -0,0 +1,154 @@
+import { Router } from "express";
+import usuarioController from "../controllers/usuarioController";
+
+const router: Router = Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Usuarios
+ *   description: Gerenciamento de Usuarios
+ */
+
+/**
+ * @swagger
+ * /usuarios:
+ *   get:
+ *     summary: Retorna todos os usuarios
+ *     tags: [Usuarios]
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios
+ *       500:
+ *         description: Erro interno do servidor
+ */
+router.get("/usuarios", usuarioController.listarTodosUsuarios);
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   get:
+ *     summary: Retorna um usuario pelo ID
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Paciente encontrado
+ *       404:
+ *         description: Paciente não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
+router.get("/usuarios/:id", usuarioController.pegarUsuarioPorId);
+
+/**
+ * @swagger
+ * /usuarios:
+ *   post:
+ *     summary: Cria um novo usuario
+ *     tags: [Usuarios]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - nome
+ *               - email
+ *               - senha
+ *               - nomeEmpresa
+ *               - tipoComercio
+ *               - cpfCnpj
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               senha:
+ *                 type: string
+ *               nomeEmpresa:
+ *                 type: string
+ *               tipoComercio:
+ *                 type: string
+ *               cpfCnpj:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Usuario criado com sucesso
+ *       400:
+ *         description: Erro na requisição
+ *       500:
+ *         description: Erro interno do servidor
+ */
+router.post("/usuarios", usuarioController.criarUsuario);
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   put:
+ *     summary: Atualiza um usuario
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               senha:
+ *                 type: string
+ *               nomeEmpresa:
+ *                 type: string
+ *               tipoComercio:
+ *                 type: string
+ *               cpfCnpj:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Usuario atualizado com sucesso
+ *       400:
+ *         description: Erro na requisição
+ *       404:
+ *         description: Usuario não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
+router.put("/usuarios/:id", usuarioController.atualizarUsuario);
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   delete:
+ *     summary: Deleta um usuario
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: Usuario deletado com sucesso
+ *       404:
+ *         description: Usuario não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
+router.delete("/usuarios/:id", usuarioController.deletarUsuario);
---
+++ src/services/usuarioService.ts
@@ -0,0 +1,32 @@
+import prisma from "../database/prisma";
+import { Usuario } from "../generated/prisma";
+
+const usuarioService = {
+    async listarTodosUsuarios(): Promise<Usuario[]> {
+        return prisma.usuario.findMany();
+    },
+
+    async pegarUsuarioPorId(id: number): Promise<Usuario | null> {
+        return prisma.usuario.findUnique({ where: { id } });
+    },
+
+    async criarUsuario(data: { nome: string; email: string; senha: string; nomeEmpresa: string; tipoComercio: string; cpfCnpj: string }): Promise<Usuario> {
+        const novoUsuario: Usuario = await prisma.usuario.create({
+            data,
+        });
+        return novoUsuario;
+    },
+
+    async atualizarUsuario(id: number, data: { nome?: string; email?: string; senha?: string; nomeEmpresa?: string; tipoComercio?: string; cpfCnpj?: string }): Promise<Usuario> {
+        return prisma.usuario.update({
+            where: { id },
+            data,
+        });
+    },
+
+    async deletarUsuario(id: number): Promise<void> {
+        await prisma.usuario.delete({ where: { id } });
+    },
+};
+
+export default usuarioService;
