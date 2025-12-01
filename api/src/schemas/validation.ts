04a844e2c76f3ebbd364dcc26d8d1f0d770f5ef5
Wálisson Andrey <85573798+PennyWASS@users.noreply.github.com>
2025-12-01 01:34:37 -0300

    Correção zod

--- api/src/schemas/validation.ts
+++ api/src/schemas/validation.ts
@@ -23,6 +23,14 @@ export const createUsuarioSchema = z.object({
     .min(2, "Nome da empresa deve ter pelo menos 2 caracteres")
     .max(150)
     .optional(), // Opcional, dependendo da sua regra de negócio
+  tipoComercio: z
+    .string()
+    .min(2, "Tipo de comércio deve ter pelo menos 2 caracteres")
+    .max(100, "Tipo de comércio deve ter no máximo 100 caracteres"),
+  cpfCnpj: z
+    .string()
+    .min(11, "CPF/CNPJ deve ter pelo menos 11 caracteres")
+    .max(14, "CPF/CNPJ deve ter no máximo 14 caracteres"),
 });
 
 // Validação específica para a rota de login
