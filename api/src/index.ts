0de1bccef769c1fa858407598bf0779ef7f06433
Wálisson Andrey <85573798+PennyWASS@users.noreply.github.com>
2025-12-04 00:51:28 -0300

    Ajustes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { Express } from "express";
-import cors from 'cors';
+// import cors from 'cors';
 import categoriaRoutes from "./routes/categoriaRoutes";
 import estoqueRoutes from "./routes/estoqueRoutes";
 import movimentacaoRoutes from "./routes/movimentacaoRoutes";
@@ -15,7 +15,7 @@ dotenv.config();
 const app: Express = express();
 const port: number = Number(process.env.PORT) || 3000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use(usuarioRoutes);
